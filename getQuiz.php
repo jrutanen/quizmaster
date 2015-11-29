e570355b1132d72c53f3008d91a230cc7836ae06
jrutanen <jani.m.rutanen@gmail.com>
2015-11-29 13:33:15 +0100

    getQuiz test

--- getQuiz.php
+++ getQuiz.php
@@ -1,8 +1,8 @@
 <?php
-/*    $servername = "localhost";
-    $username = "";
-    $password = "";
-
+    $servername = "localhost";
+    $username = "test";
+    $password = "test";
+/*
     //create connection
     $conn = new mysqli($servername, $username, $password);
 
