f55bb5640a093e296408049e782faa95099de391
jrutanen <jani.m.rutanen@gmail.com>
2015-11-29 17:43:00 +0100

    updates to running.php

--- running.php
+++ running.php
@@ -1,14 +1,27 @@
 <?php
+    ini_set('display_errors', 1);
     $server = 'localhost';
     $username = 'web';
+    $dbname = 'quiz_db';
 
     //Create connection
-    $conn = new mysqli($server, $username, 'pw');
+    $conn = new mysqli($server, $username, 'pw', $dbname);
 
     //check connection
     if ($conn->connect_error) {
         die ("Connection failed: " . $conn->connect_error);
     }
 
-    echo "Connected successfully";
+    $sql = "SELECT running FROM game WHERE id=1";
+
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        echo $row["running"];
+    } else {
+        echo "ERROR - No running status available.";
+    }
+
+    $conn->close();
 ?>
