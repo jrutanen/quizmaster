79f5aba044b12f28753e1d04b8a43bb3f25b587d
jrutanen <jani.m.rutanen@gmail.com>
2015-11-29 11:57:45 +0100

    added db connection

---
+++ getQuiz.php
@@ -0,0 +1,15 @@
+<?php
+    $servername = "localhost";
+    $username = "";
+    $password = "";
+
+    //create connection
+    $conn = new mysqli($servername, $username, $password);
+
+    //check connection
+    if ($conn->mysqli_connect_error()) {
+        die("Connection failed: " . $conn->mysqli_connect_error()):
+    }
+
+    echo "Connected successfully";
+?>
