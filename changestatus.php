81fcc3fd0db7e1df239fd76201dc194fa09ff2f4
jrutanen <jani.m.rutanen@gmail.com>
2015-11-29 19:36:02 +0100

    adding game controller

---
+++ changestatus.php
@@ -0,0 +1,35 @@
+<?php
+    ini_set('display_errors', 1);
+    if(isset($_GET['status'])) {
+        $status = ($_GET["status"]);
+        if ($status == 0) {
+            $sql = "UPDATE game SET running=0 WHERE id=0";
+        } else {
+            $sql = "UPDATE game SET running=1 WHERE id=0";
+        }
+
+        $server = 'localhost';
+        $username = 'web';
+        $dbname = 'quiz_db';
+
+        //Create connection
+        $conn = new mysqli($server, $username, 'pw', $dbname);
+
+        //check connection
+        if ($conn->connect_error) {
+            die ("Connection failed: " . $conn->connect_error);
+        }
+
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            echo "ok";
+        } else {
+            echo "ERROR - No was not able to change value.";
+        }
+
+        $conn->close();
+    } else {
+        echo "ERROR - No Parameter given.";
+    }
+?>
