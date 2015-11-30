3c605f404e1a9efe62ef175f037c1284d19927da
jrutanen <jani.m.rutanen@gmail.com>
2015-11-30 21:04:45 +0100

    fixed another bug in getresults.php

--- getresults.php
+++ getresults.php
@@ -1,4 +1,3 @@
-//select player, question, count(correct) from results group by player, question;
 <?php
     include 'db.php';
     $sql = mysqli_query($conn, "SELECT player, question, count(correct) "
@@ -6,14 +5,9 @@
                               ."GROUP BY player, question");
 
     $rows = array();
-    $i = 0;
+
     while($row = mysqli_fetch_assoc($sql)) {
-        if($i == 0) {
-            print $row;
-            $i = 1;
-        } else {
-            $rows[] = $row;
-        }
+        $rows[] = $row;
     }
     print json_encode($rows);
 
